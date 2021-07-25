7080ca50a19fbca5275e61ecbf33c04d2957370e
Jacob72282 <77362318+Jacob72282@users.noreply.github.com>
2021-07-24 21:32:11 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+var hour;
+
+var bg = "sunrise.png";
+
+function preload() {
+    getBackgroundImg();
+    
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+    if(backgroundImg)
+        background(backgroundImg);
+
+    Engine.update(engine);
+
+   
+    
+    
+}
+
+async function getBackgroundImg(){
+
+    // write code to fetch time from API
+    
+ 
+    //change the data in JSON format and store it in variable responseJSON
+    
+
+    
+    //fetch datetime from responseJSON
+    
+    
+
+    // slice the datetime to extract hour
+    
+
+    
+    if(hour>=0 && hour<18 ){
+        bg = "sunrise.png";
+    }
+    else{
+        bg="sunset.png"
+    }
+    
+    backgroundImg = loadImage(bg);
+}
